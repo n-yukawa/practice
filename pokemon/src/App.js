a82fcbcdabfc6ec8bbeb670bc7a798cf6c7565b4
Nanoko Yukawa <yukawa.nanoko@gmail.com>
2024-01-05 13:05:54 +0900

    wip ピカチュウの画像と名前を取得

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -2,16 +2,18 @@ import React from 'react'
 import './App.css';
 import PokemonList from "./PokemonList";
 import PokemonPickup from "./PokemonPickup";
+import PokemonData from './PokemonData';
 
 function App() {
   // const font = css`
   //   text-transform: none;
   // `;
+
   return (
     <div className='App'>
         <h1>ポケモン図鑑</h1>
         <PokemonPickup />
-        <PokemonList />
+        <PokemonData />
     </div>
   );
 }
--- todoapp/src/Todo.js
+++ todoapp/src/Todo.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
+import Button from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import './Icons.css'
 
-const Todo = ({ todo, toggleTodo }) => {
+const Todo = ({ todo, toggleTodo, setTodos }) => {
     const handleTodoClick = () => {
         toggleTodo(todo.id);
     };
+    const handleDelete = () => {
+        const newTodos = 
+        setTodos(newTodos);
+    };
+
+    // const onClickDelete = () => {
+    //     alert("削除");
+    // };
+
 
     return (
-        <div>
+        <div className="wrap">
             <label>
                 <input 
                     type="checkbox" 
@@ -16,6 +28,14 @@ const Todo = ({ todo, toggleTodo }) => {
                 />
             </label>
             {todo.name}
+            {/* ここから追加 */}
+            <div className="icon_area"
+                onClick={handleDelete}
+            >
+                <DeleteIcon className="icon" 
+                />
+                <span className="icon_text">削除</span>
+            </div>
         </div>
     );
 };

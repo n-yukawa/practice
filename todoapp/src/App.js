ab55f8c28b5bb4e7c2477db457a23fbc8ae0cc8e
Nanoko Yukawa <141843095+n-yukawa@users.noreply.github.com>
2024-01-28 11:27:19 +0900

    [remove]不要なコメントの削除

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,9 +2,7 @@ import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 import './App.css';
 import { v4 as uuidv4 } from "uuid";
-//uuidのインポートの仕方はドキュメントを確認
 
-//MUIを追加
 import { Button } from "@mui/material";
 import Stack from '@mui/material/Stack';
 import AddIcon from '@mui/icons-material/Add';
@@ -12,33 +10,20 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  // useState->変数を監視・管理するために使われているHooks
-  // todos->これから追加していくタスク（オブジェクト）
-  //todoを追加したタイミングで画面自体が再レンダリング（更新）される
-  //todosの変数が変わったときだけページを更新することができるため、無駄な再レンダリングを防げる
 
   const todoNameRef = useRef();
-  //まず、入力された文字列を取得するために、useRef（Hooks）を追加する
 
   const handleAddTodo = () => {
-    //タスクを追加する
     const name = todoNameRef.current.value;
     if(name === "") return;
-    //入力値が空の場合は終了
     setTodos((prevTodos) => {
       return [...prevTodos, {id: uuidv4(), name: name, completed: false }];
     });
     todoNameRef.current.value = null;
-    //入力が終わったら、値をnullにする
   };
-    //...prevTodos->スプレッド構文
-    //prevTodos = previousTodos = 「前のタスク」の状態に対して、新しいタスクを追加していく
-    //id: uuidv4()->ランダムな文字列がidに割り振られる
 
   const toggleTodo = (id) => {
     const newTodos = [...todos];
-    //todosの中のオブジェクトをnewTodosにコピーしている
-    //状態変数で管理されているtodosを直接触るのはあまり好ましくない
     const todo = newTodos.find((todo) => todo.id === id);
     todo.completed = !todo.completed;
     setTodos(newTodos);
@@ -63,7 +48,6 @@ function App() {
   };
 
   return (
-    //最初のtodosはprops。todosという名前で渡してね。{}の中が渡したい変数名
     <div className={"main-wrap"}>
       <h1>Todo List</h1>
       <div className="input-wrap">
--- todoapp/src/TodoList.js
+++ todoapp/src/TodoList.js
@@ -3,11 +3,6 @@ import Todo from "./Todo";
 
 const TodoList = ( { todos, toggleTodo, handleDelete, handlePriorityChange }) => {
   return todos.map((todo) => <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} handleDelete={handleDelete} handlePriorityChange={handlePriorityChange} />);
-  //map関数->todosの配列の中身を一つ一つ取り出す
-  //(todo)変数に入れて、<Todo />コンポーネントに渡す
-  //key={todo} ->ユニークなkeyである必要があるため、あまり好ましくない書き方（同じ名前のタスクがあると、keyが重複してしまう）
-  //key uuid（ライブラリ）を使うと重複が起こらない
-    //ターミナルで「npm install uuid」を入力し、インストールする。package.jsonにuuidが追加されている。
 };
 
 export default TodoList
